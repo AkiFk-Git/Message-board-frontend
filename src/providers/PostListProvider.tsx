a8e8ada9627c140c84a0d5e3f0fe9929f4b316e1
akiGIt <naofk221512@gmail.com>
2026-01-06 17:01:32 +0900

    providerの型を定義

--- src/providers/PostListProvider.tsx
+++ src/providers/PostListProvider.tsx
@@ -1,4 +1,4 @@
-import { useState, createContext, Dispatch, SetStateAction } from "react";
+import { useState, createContext, Dispatch, SetStateAction, ReactNode } from "react";
 
 // ポストを保持する型を定義
 export type PostType = {
@@ -8,6 +8,12 @@ export type PostType = {
   content: string;
   createdAt: Date;
 };
+
+// PoviderのPropsを型定義
+interface UserProviderProps {
+  children: ReactNode;
+}
+
 //　PostListContextの定義
 export const PostListContext = createContext(
   {} as {
@@ -16,7 +22,7 @@ export const PostListContext = createContext(
   },
 );
 //　PostListProviderの定義
-export const PostListProvider = (props: any) => {
+export const PostListProvider = (props: UserProviderProps) => {
   const { children } = props;
   const [postList, setPostList] = useState<PostType[]>([]);
   return (
--- src/providers/UserProvider.tsx
+++ src/providers/UserProvider.tsx
@@ -1,10 +1,15 @@
-import { useState, createContext, Dispatch, SetStateAction } from "react";
+import { useState, createContext, Dispatch, SetStateAction, ReactNode } from "react";
 
 // 保持する情報の型
 type UserInfo = {
   userUuid: string;
   token: string;
 };
+
+// PoviderのPropsを型定義
+interface UserProviderProps {
+  children: ReactNode;
+}
 
 // UserContextの作成
 export const UserContext = createContext(
@@ -15,7 +20,7 @@ export const UserContext = createContext(
 );
 
 // UserProviderの定義
-export const UserProvider = (props: any) => {
+export const UserProvider = (props: UserProviderProps) => {
   const { children } = props;
   const [userInfo, setUserInfo] = useState<UserInfo>({ userUuid: "", token: "" });
   return (
