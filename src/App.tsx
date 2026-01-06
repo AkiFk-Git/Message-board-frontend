1e7c43e85e453b7ec86e2a956baca095a7a90599
akiGIt <naofk221512@gmail.com>
2026-01-06 16:21:29 +0900

    AppLayout追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,13 @@ import SignIn from './pages/SignIn';
 import Main from './pages/Main';
 import { UserProvider } from "./providers/UserProvider";
 import SignUp from './pages/SignUp';
+import AppLayout from './pages/AppLayout';
 
 
 function App() {
   return (
     <div className="App">
-      <UserProvider>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/signUp" element={<SignUp />} />
-        </Routes>
-      </UserProvider>
+      <AppLayout/>
     </div>
   );
 }
--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../components/Header";
+import { UserProvider } from "../providers/UserProvider";
+import SignIn from "../components/SignIn";
+import Main from "./Main";
+import SignUp from "./SignUp";
 
 export default function AppLayout() {
     return(
-        <Header/>
+        <UserProvider>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<SignIn />} />
+                <Route path="/main" element={<Main />} />
+                <Route path="/signUp" element={<SignUp />} />
+            </Routes>
+      </UserProvider>
     )
 }
